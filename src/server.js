145d76cc43c3bd1e3898e75d2d363ef338e5511a
Maryna <marin.naumch@gmail.com>
2026-01-01 01:01:38 +0200

    finish tasks

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import { errors } from "celebrate";
+import cookieParser from 'cookie-parser';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
@@ -17,6 +19,9 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);         // 1. Логер першим — бачить усі запити
 app.use(express.json()); // 2. Парсинг JSON-тіла
 app.use(cors());         // 3. Дозвіл для запитів з інших доменів
+app.use(cookieParser());
+
+app.use(authRoutes);
 app.use(notesRoutes);
 
 
